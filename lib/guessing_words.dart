a8439eaf8ec1fdd1bbb860509316f2da879d12fc
Juan Gabriel Gutierrez <correo.del.llanox@gmail.com>
2019-09-05 00:20:40 -0500

    - Added gesture Drag End to mark the word as the correct one or to pass to the next one. - Added support to gyroscope events so we can now listen to the changes when the smartphone changes the position in the space. However, we still need to improve accuracy when detecting the event of spinning forward or backward. - Created one file per screen since we were keeping all source code in the same .dart file. So, at that way, we can get something understandable and maintainable.

--- lib/guessing_words.dart
+++ lib/guessing_words.dart
@@ -9,90 +9,131 @@ import 'game_data.dart';
 class GuessingWordsWidget extends StatefulWidget {
   final Category category;
   final int startTime;
+
   GuessingWordsWidget({Key key, this.category, this.startTime})
       : super(key: key);
+
   State createState() => _GuessingWordsState();
 }
 
 class _GuessingWordsState extends State<GuessingWordsWidget> {
-  bool hasTimerStopped = false;
-  bool hasPassed = false;
-  bool hasWon = false;
-  List<double> _accelerometerValues;
-  List<StreamSubscription<dynamic>> _streamSubscriptions =
-  <StreamSubscription<dynamic>>[];
+  bool _hasTimerStopped = false;
+  bool _hasPassed = false;
+  bool _isCorrect = false;
+
 
   @override
   void initState() {
     super.initState();
 
-//    _streamSubscriptions
-//        .add(accelerometerEvents.listen((AccelerometerEvent event) {
-//      setState(() {
-//
-//        if( event.x < 7 && event.y > 7 ){
-//          hasPassed = true;
-//          hasWon = false;
-//        }
-//
-//        if( event.x < 7 && event.y > -6 ){
-//          hasPassed = false;
-//          hasWon = true;
-//        }
-//      });
-//    }));
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      // print('Output x:[ ${event.x} ] y:[ ${event.y} ] z:[ ${event.z} ]');
 
-  }
+      if (event.y < -5 && event.x < 0 && event.z < 0) {
+        print('Correct x:[ ${event.x} ] y:[ ${event.y} ] z:[ ${event.z} ]');
+        setState(() {
+          _isCorrect = true;
+          _hasPassed = false;
 
-  @override
-  void dispose() {
-    super.dispose();
-    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
-      subscription.cancel();
-    }
+        });
+      }
+
+      if (event.y > 5 && event.x > 0 && event.z > 0.5) {
+        print('Pass it x:[ ${event.x} ] y:[ ${event.y} ] z:[ ${event.z} ]');
+        setState(() {
+          _isCorrect = false;
+          _hasPassed = true;
+
+        });
+      }
+    });
   }
 
 
   @override
   Widget build(BuildContext context) {
-    final List<String> accelerometer =
-    _accelerometerValues?.map((double v) => v.toStringAsFixed(1))?.toList();
 
-    var messageToShow = hasTimerStopped? "Game Over!!" : selectRandomWord(widget.category.words);
-    var messageWidget = Text(
-      messageToShow,
-      style: TextStyle(color: hasTimerStopped? Colors.red : Colors.white, fontSize: 54.0),
+    var timerWidget =  CountDownTimer(
+      secondsRemaining: widget.startTime,
+      whenTimeExpires: () {
+        setState(() {
+          _hasTimerStopped = true;
+        });
+      },
+      countDownTimerStyle: TextStyle(
+          color: Colors.white,
+          backgroundColor: Colors.blue,
+          fontSize: 54.0),
+    );
+
+    var raisedButton =  RaisedButton(
+      padding: const EdgeInsets.all(8.0),
+      textColor: Colors.white,
+      color: Colors.blue,
+      onPressed: (){
+        _playAgain(context);
+        },
+      child: new Text("Play Again",style: TextStyle(
+          color: Colors.white, fontSize: 54.0),),
     );
 
 
+    var dynamicWidget = _hasTimerStopped ? raisedButton : timerWidget;
+
+
+    var messageToShow = _hasTimerStopped
+        ? "Time Up!!"
+        : _selectRandomWord(widget.category.words);
+
+    var messageColor = _hasTimerStopped? Colors.red: Colors.white;
+
+
+   if(_hasPassed){
+      messageToShow = "Pass it";
+      messageColor = Colors.yellow;
+   }
+
+   if(_isCorrect){
+     messageToShow = "Correct!!";
+     messageColor = Colors.lightGreen;
+   }
+
+
+    var messageWidget = Text(
+      messageToShow,
+      style: TextStyle(
+          color: messageColor, fontSize: 54.0),
+    );
+
     return GestureDetector(
+        onDoubleTap: (){
+          _showANewWord();
+        },
         onHorizontalDragEnd: (details) {
           _goBack(context, details);
         },
-        child:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          CountDownTimer(
-            secondsRemaining: widget.startTime,
-            whenTimeExpires: () {
-              setState(() {
-                hasTimerStopped = true;
-              });
-            },
-            countDownTimerStyle: TextStyle(
-                color: Colors.white,
-                backgroundColor: Colors.blue,
-                fontSize: 54.0),
+        onVerticalDragEnd: (details) {
+          _processGesture(details);
+        },
+        child:  Container(
+          height: 150,
+          width: 850,
+          child: Center(
+            child:   Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              dynamicWidget,
+              Container(
+                height: 150,
+                width: 850,
+                child: Center(
+                  child: messageWidget,
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0), color: Colors.blue),
+              )
+            ]),
           ),
-          Container(
-            height: 150,
-            width: 850,
-            child: Center(
-              child: messageWidget,
-            ),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0), color: Colors.blue),
-          )
-        ]));
+        )
+    );
   }
 
   void _goBack(BuildContext context, DragEndDetails details) {
@@ -102,11 +143,42 @@ class _GuessingWordsState extends State<GuessingWordsWidget> {
     }
   }
 
-  String selectRandomWord(List<String> words) {
+  void _playAgain(BuildContext context) {
+      Navigator.pop(context);
+  }
+
+  String _selectRandomWord(List<String> words) {
     var randomGenerator = new Random();
     return words[randomGenerator.nextInt(words.length)];
   }
 
+  void _processGesture(DragEndDetails details) {
+    if(_hasTimerStopped) return;
+    var velocity = details.primaryVelocity;
+
+    if (velocity > 0) {
+      setState(() {
+        print("+ ");
+        _isCorrect = true;
+        _hasPassed = false;
+      });
+    } else {
+      print("- ");
+      setState(() {
+        _isCorrect = false;
+        _hasPassed = true;
+      });
+    }
+  }
+
+  void _showANewWord() {
+    print('Double Tap ${_hasTimerStopped}');
+    if(_hasTimerStopped) return;
+    setState(() {
+      _isCorrect = false;
+      _hasPassed = false;
+      _hasTimerStopped = false;
+    });
+
+  }
 }
-
-
